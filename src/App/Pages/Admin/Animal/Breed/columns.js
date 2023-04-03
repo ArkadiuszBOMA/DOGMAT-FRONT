0416d322159ab9d674bcf1e52d0a95b39e0b7d8c
Arkadiusz Bojara <bojara.arkadiusz@gmail.com>
2023-04-03 18:43:57 +0200

    Korekta błędnie wyświetlanej daty w przypadku kiedy w bazie jest null. Zmiana formatowania komórki Cell w tabelach kolumn przekazywanych do tworzenia tabeli dynamicznej.

---
+++ src/App/Pages/Admin/Animal/Breed/columns.js
@@ -0,0 +1,58 @@
+import {format} from "date-fns"
+export const COLUMNS = [
+	{
+		Header: 'ID',
+		Footer: 'ID',
+		accessor: 'id',
+		isEditable: false,
+		disableFilters: true,
+	},
+	{
+		Header: 'Rasa',
+		Footer: 'Rasa',
+		isEditable: false,
+		accessor: 'name',
+	},
+	{
+		Header: 'Zwierzę',
+		Footer: 'Zwierzę',
+		isEditable: false,
+		accessor: 'animalType',
+	},
+	{
+		Header: 'Daty',
+		Footer: 'Daty',
+		columns:
+		[
+			{
+				Header: 'Utworzenia',
+				Footer: 'Utworzenia',
+				accessor: 'date_create',
+				isEditable: false,
+				Cell: ({ value }) => {return value !== null ? format(new Date(value), 'dd-MM-yyyy') : " "},
+			},
+			{
+				Header: 'Modyfikacji',
+				Footer: 'Modyfikacji',
+				accessor: 'date_modify',
+				isEditable: false,
+				Cell: ({ value }) => {return value !== null ? format(new Date(value), 'dd-MM-yyyy') : " "},
+			},
+			{
+				Header: 'Archiwizacji',
+				Footer: 'Archiwizacji',
+				accessor: 'date_archive',
+				isEditable: false,
+				Cell: ({ value }) => {return value !== null ? format(new Date(value), 'dd-MM-yyyy') : " "},
+
+			}
+		]
+	},
+	{
+		id:'archive',
+		Header: 'Archiwizowane?',
+		Footer: 'Archiwizowane?',
+		isEditable: false,
+		accessor: d => { return d ? 'W archiwum' : 'Używane' },
+	},
+]
